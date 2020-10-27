4ed43becebb4e32d3e97281582a90075fdbac23e
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-27 02:10:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,4 +201,5 @@ end
 def big_shoe_rebounds
   home_shoes = get_player_shoes(game_hash[:home][:players])
   away_shoes = get_player_shoes(game_hash[:away][:players])
-  all_shoes = home_shoes 
+  all_shoes = home_shoes + away_shoes
+   
